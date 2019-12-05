8f788aa3471bb9f86d5c0066ef33329a4540f9dd
meholt <meholt90@gmail.com>
2019-12-04 16:26:53 -0800

    initial setup of most components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+const toDoList = [ {name: '', id: '', completed: false} ];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+
   render() {
+
+    console.log('...rendering...');
+
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="">
+        <h2>Welcome to your To-Do List!</h2>
       </div>
     );
   }
